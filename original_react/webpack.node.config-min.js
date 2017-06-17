47045d098967e624aa44a4941ff435af8d1380f1
benpurslow <b.purslow@famegroup.com>
2017-06-17 12:03:39 +0100

    removing redundant webpack. May add back later if I decide to create a server-side section.

--- original_react/webpack.node.config-min.js
+++
@@ -1,1 +0,0 @@
-"use strict";var webpack=require("webpack"),path=require("path"),nodeModules={};module.exports={name:"server",target:"node",entry:"./server/serverEntry.js",output:{path:"./bin/",publicPath:"bin/",filename:"serverEntry.js"},externals:nodeModules,module:{loaders:[{test:/\.js$/,loaders:["babel-loader"]},{test:/\.json$/,loader:"json-loader"}]}};
