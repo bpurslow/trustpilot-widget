fcfe4fa61e0b9016938496b2e5414a96bb8b2433
benpurslow <b.purslow@famegroup.com>
2017-06-19 12:55:49 +0100

    Separated out business logic from UI. Implemented promise when gathering Review data from the new review service. Removed work done by Review component and handed it to it's parent (ReviewContainer) Review.js is now super basic

--- original_react/client/components/Reviews/ReviewContainer.js
+++ original_react/client/components/Reviews/ReviewContainer.js
@@ -1,112 +1,77 @@
+/**
+ * Created by benpurslow on 13/06/2017.
+ */
+
+//Core Imports
 const React = require('react');
 
+//Utils
+import {sleep} from '../../utils/utils';
+
+//Services
+import {getReviews} from '../../services/Review';
+
+//Components
 import Review from './Review';
-import Loader from "./Loader";
+import Loader from '../Loader';
 
 
 export class ReviewContainer extends React.Component{
     constructor(props) {
         super(props);
 
-        let reviews = this._getReviews();
         this.state = {
-            reviews: reviews,
-            loading: true
-        }
+            reviews: {},
+            loading: true,
+            activeReview: 0,
+            content: <Loader/>
+        };
+
+        // Bind this to click event
+        this.setActive = this.setActive.bind(this);
     }
 
     componentDidMount() {
-        setTimeout(() => {
-            this._reviews = this._renderReviews(this.state.reviews);
-            this.setState({loading: false});
-        }, 2500);
+        // Only on mount are the reviews grabbed
+        let self = this;
+        getReviews().then( function (reviews) {
+            // This will get triggered even if the widget is closed
+            // Can only think of checking to see if the widget is open or not right now...
+            // Point is I KNOW THIS IS AN ISSUE I SWEAR!
+            self.setState({reviews: reviews});
+        }).then( function() {
+            self.setState({loading: false, content: self._renderReviews(self.state.reviews)});
+        })
     }
 
     _renderReviews(reviews) {
         if (reviews.length > 0) {
             return reviews.map((review,index) => (
-                <Review key={index} review={review}/>
+                <Review
+                    key={index}
+                    review={review}
+                    active={this.state.activeReview === review.id}
+                    onActive={this.setActive}
+                />
         ));
+        } else {
+            return <div className="review--none">No Reviews Available</div>
+        }
+    }
+
+    setActive(reviewId) {
+        if (reviewId === this.state.activeReview) {
+            this.setState({activeReview: 0});
+        } else {
+            this.setState({activeReview: reviewId});
         }
     }
 
     render() {
-        let content = null;
-        if (this.state.loading) {
-            content = <Loader/>
-        } else {
-            content = this._reviews;
-        }
-
         return (
             <section className="reviews">
-                {content}
+                {this.state.loading? <Loader/>: this._renderReviews(this.state.reviews)}
             </section>
         )
-    }
-
-    _getReviews(limit) {
-        let reviews = [];
-
-        if (!limit) {
-            limit = 5;
-        }
-
-        let first_name = [
-            "Simon",
-            "Gav",
-            "Justin",
-            "Erika",
-            "Hugo",
-            ""
-        ];
-        let last_name = [
-            "Lock",
-            "Wright",
-            "Wolfe",
-            "Beja",
-            ""
-        ];
-        let review_title = [
-            "Super quality.. I will show here again!",
-            "Princely Sum",
-            "Good Services",
-            "Nightmare experience - no product, no communication, no refund; improved by rapid resolution",
-            "FRAUD"
-        ];
-        let review_body = [
-            "Super nice quality, fast devilery, good prices. I will shop here again!",
-            "A decent local curry house in Faversham, Kent known for its Elvis nights.",
-             "In early 2012, I ordered a set of chairs from Infurn. I thought that by ordering in March, I would have what I needed by November, certainly. I wanted the perfect chairs for my house, and really did not NEED them before the annual Thanksgiving dinner (the only time of year I have a need for a whole lot of chairs at one time). I played it safe, I thought, by ordering so far in advance. \
-                                    Week after week, month after month, Infurn's website kept updating the status of the order so that the chairs' arrival would be further and further in the distant future. Finally when the week of Thanksgiving arrived, and I had had contact with their customer service in early November (and their only clueless reply was to say, 'We had manufacturing problems; let's hope the chairs arrive this week as scheduled.'), I still had no chairs, had to go out and buy some other chairs and still had no clue when the Infurn chairs might arrive. \
-                                    At some point I finally just requested a refund because Infurn could neither deliver my chairs nor give me a solid date about when I might receive them when I inquired about a delivery date. They finally offered me a refund - which I accepted on 14 December 2012.",
-            "I've been patiently waiting for a miracle to happen with our order AU-316084, 12 Chairs!! First the delays, lots and lots of delays with no apparent reason... after they send just 1/2 of the order and say it's all... So we bought 'Pairs' of chairs... we paid for 5 PAIRS and received 5 chairs!!! The other 2, the rocket chairs we never receive.... \
-                                    Communication ZERO, they simply ignore the e-mails... when they did respond they asked us to prove our order to be pairs!!! LOL and just stopped communicating... their website is constantly down... probably to make lose interest and rest your forces to recover what you paid for!!",
-            "A decent place to introduce your taste buds to fiery Indian fare"
-        ];
-        let star_rating = ["1", "2", "3", "4", "5" ];
-        let review_reply = [
-            "We are SOOOOOOO sorry /s",
-            "This is completely unacceptable and we apologise profusely. Please visit again and we'll make sure this never happens again",
-            undefined,
-            undefined,
-            "Meh..."
-        ];
-
-        for (let n = 0; n < limit; ++ n) {
-            let first_n = first_name[Math.floor(Math.random() * first_name.length)];
-            let last_n = last_name[Math.floor(Math.random() * last_name.length)];
-            reviews.push({
-                "firstName": first_n,
-                "lastName": last_n,
-                "fullName": first_n + " " + last_n,
-                "reviewTitle": review_title[Math.floor(Math.random() * review_title.length)],
-                "reviewBody": review_body[Math.floor(Math.random() * review_body.length)],
-                "reviewStars": star_rating[Math.floor(Math.random() * star_rating.length)],
-                "reviewReply": review_reply[Math.floor(Math.random() * review_reply.length)]
-            })
-        }
-
-        return reviews;
     }
 }
--- original_react/client/services/Review.js
+++ original_react/client/services/Review.js
@@ -1,105 +1,79 @@
 /**
  * Created by benpurslow on 11/06/2017.
  */
-const utils = require('../utils/utils.js');
 
-export function getCompany(args) {
-    args
-    let company = [];
+import {sleep} from '../utils/utils.js';
 
-    //Fake trustscore
-    let trustscore = utils.randomInt(0, 10);
-    let starRating = Math.ceil(trustscore/2);
-
-    company.pop({
-        "companyTitle": "Fake widget LTD",
-        "trustscore": trustscore.toFixed(1),
-        "totalReviews": "1230",
-        "starRating": starRating.toString()
-    });
-
-  return company;
-}
-
-export function getReviews(args) {
+export function getReviews(number_of_reviews) {
     /**
      * Call to fake api
      * Get response and parse
-     * url: Url for API
      * number_of_reviews: Int to determine number of fake reviews
      */
+    return sleep(5000)
+        .then(function () {
 
-    // Override
-    let number_of_reviews = 5;
+            let reviews = [];
 
-    let reviews = [];
+            if (!number_of_reviews) {
+                number_of_reviews = 5;
+            }
 
-    let first_name = [
-        "Simon",
-        "Gav",
-        "Justin",
-        "Erika",
-        "Hugo",
-        ""
-    ];
-    let last_name = [
-        "Lock",
-        "Wright",
-        "Wolfe",
-        "Beja",
-        ""
-    ];
-    let review_title = [
-        "Super quality.. I will show here again!",
-        "Princely Sum",
-        "Good Services",
-        "Nightmare experience - no product, no communication, no refund; improved by rapid resolution",
-        "FRAUD"
-    ];
-    let review_body = [
-        "Super nice quality, fast devilery, good prices. I will shop here again!",
-        "A decent local curry house in Faversham, Kent known for its Elvis nights.",
-         "In early 2012, I ordered a set of chairs from Infurn. I thought that by ordering in March, I would have what I needed by November, certainly. I wanted the perfect chairs for my house, and really did not NEED them before the annual Thanksgiving dinner (the only time of year I have a need for a whole lot of chairs at one time). I played it safe, I thought, by ordering so far in advance. \
-                                Week after week, month after month, Infurn's website kept updating the status of the order so that the chairs' arrival would be further and further in the distant future. Finally when the week of Thanksgiving arrived, and I had had contact with their customer service in early November (and their only clueless reply was to say, 'We had manufacturing problems; let's hope the chairs arrive this week as scheduled.'), I still had no chairs, had to go out and buy some other chairs and still had no clue when the Infurn chairs might arrive. \
-                                At some point I finally just requested a refund because Infurn could neither deliver my chairs nor give me a solid date about when I might receive them when I inquired about a delivery date. They finally offered me a refund - which I accepted on 14 December 2012.",
-        "I've been patiently waiting for a miracle to happen with our order AU-316084, 12 Chairs!! First the delays, lots and lots of delays with no apparent reason... after they send just 1/2 of the order and say it's all... So we bought 'Pairs' of chairs... we paid for 5 PAIRS and received 5 chairs!!! The other 2, the rocket chairs we never receive.... \
-                                Communication ZERO, they simply ignore the e-mails... when they did respond they asked us to prove our order to be pairs!!! LOL and just stopped communicating... their website is constantly down... probably to make lose interest and rest your forces to recover what you paid for!!",
-        "A decent place to introduce your taste buds to fiery Indian fare"
-    ];
-    let star_rating = ["1", "2", "3", "4", "5" ];
-    let review_reply = [
-        "We are SOOOOOOO sorry /s",
-        "This is completely unacceptable and we apologise profusely. Please visit again and we'll make sure this never happens again",
-        undefined,
-        undefined,
-        "Meh..."
-    ];
+            let first_name = [
+                "Simon",
+                "Gav",
+                "Justin",
+                "Erika",
+                "Hugo"
+            ];
+            let last_name = [
+                "Lock",
+                "Wright",
+                "Wolfe",
+                "Beja",
+                ""
+            ];
+            let review_title = [
+                "Super quality.. I will show here again!",
+                "Princely Sum",
+                "Good Services",
+                "Nightmare experience - no product, no communication, no refund; improved by rapid resolution",
+                "FRAUD"
+            ];
+            let review_body = [
+                "Super nice quality, fast devilery, good prices. I will shop here again!",
+                "A decent local curry house in Faversham, Kent known for its Elvis nights.",
+                "In early 2012, I ordered a set of chairs from Infurn. I thought that by ordering in March, I would have what I needed by November, certainly. I wanted the perfect chairs for my house, and really did not NEED them before the annual Thanksgiving dinner (the only time of year I have a need for a whole lot of chairs at one time). I played it safe, I thought, by ordering so far in advance. \
+                                       Week after week, month after month, Infurn's website kept updating the status of the order so that the chairs' arrival would be further and further in the distant future. Finally when the week of Thanksgiving arrived, and I had had contact with their customer service in early November (and their only clueless reply was to say, 'We had manufacturing problems; let's hope the chairs arrive this week as scheduled.'), I still had no chairs, had to go out and buy some other chairs and still had no clue when the Infurn chairs might arrive. \
+                                       At some point I finally just requested a refund because Infurn could neither deliver my chairs nor give me a solid date about when I might receive them when I inquired about a delivery date. They finally offered me a refund - which I accepted on 14 December 2012.",
+                "I've been patiently waiting for a miracle to happen with our order AU-316084, 12 Chairs!! First the delays, lots and lots of delays with no apparent reason... after they send just 1/2 of the order and say it's all... So we bought 'Pairs' of chairs... we paid for 5 PAIRS and received 5 chairs!!! The other 2, the rocket chairs we never receive.... \
+                                        Communication ZERO, they simply ignore the e-mails... when they did respond they asked us to prove our order to be pairs!!! LOL and just stopped communicating... their website is constantly down... probably to make lose interest and rest your forces to recover what you paid for!!",
+                "A decent place to introduce your taste buds to fiery Indian fare"
+            ];
+            let star_rating = ["1", "2", "3", "4", "5"];
+            let review_reply = [
+                "We are SOOOOOOO sorry /s",
+                "This is completely unacceptable and we apologise profusely. Please visit again and we'll make sure this never happens again",
+                undefined,
+                undefined,
+                "Meh..."
+            ];
 
-    for (let n = 0; n < number_of_reviews; ++ n) {
-        let first_n = first_name[Math.floor(Math.random() * first_name.length)];
-        let last_n = last_name[Math.floor(Math.random() * last_name.length)];
-        reviews.push({
-            "firstName": first_n,
-            "lastName": last_n,
-            "fullName": first_n + " " + last_n,
-            "reviewTitle": review_title[Math.floor(Math.random() * review_title.length)],
-            "reviewBody": review_body[Math.floor(Math.random() * review_body.length)],
-            "starRating": star_rating[Math.floor(Math.random() * star_rating.length)],
-            "reviewReply": review_reply[Math.floor(Math.random() * review_reply.length)]
-        })
-    }
+            for (let n = 0; n < number_of_reviews; ++n) {
+                let first_n = first_name[Math.floor(Math.random() * first_name.length)];
+                let last_n = last_name[Math.floor(Math.random() * last_name.length)];
+                reviews.push({
+                    "id": n + 1,
+                    "firstName": first_n,
+                    "lastName": last_n,
+                    "fullName": first_n + " " + last_n,
+                    "reviewTitle": review_title[Math.floor(Math.random() * review_title.length)],
+                    "reviewBody": review_body[Math.floor(Math.random() * review_body.length)],
+                    "reviewStars": star_rating[Math.floor(Math.random() * star_rating.length)],
+                    "reviewReply": review_reply[Math.floor(Math.random() * review_reply.length)]
+                })
+            }
+
+            return reviews;
+        });
 }
-
-export default function load(func, args) {
-  return new Promise((resolve, reject) => {
-    // make async call to database
-    setTimeout(() => {
-      if (Math.random() < 0.33) {
-        reject('Widget load fails 33% of the time. You were unlucky.');
-      } else {
-        resolve(func(args));
-      }
-    }, 1000); // simulate async load
-  })
-}
-
