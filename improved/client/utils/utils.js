5184889f148a08657f80fadfc155537150bd744d
benpurslow <b.purslow@famegroup.com>
2017-06-22 14:13:28 +0100

    Removed generic calls to trustpilot

--- improved/client/utils/utils.js
+++ improved/client/utils/utils.js
@@ -1,4 +1,4 @@
-import {API_KEY, BASE_URL} from '../consts';
+
 
 export function randomInt(min,max) {
     return (Math.random()*(max-min)+min);
@@ -14,32 +14,4 @@ export function getStarImage(starRating) {
     } catch(err) {
         return require('../img/star-ratings/na-stars.png');
     }
-}
-
-export function getTrustpilotData(args) {
-    return new Promise((resolve, reject) => {
-        let api_url = (typeof args.api_path !== undefined ? args.api_path : "");
-        let params = (typeof args.params !== undefined ? args.params : "");
-        let query = "";
-
-        // Thank you stackoverflow! ;)
-        // https://stackoverflow.com/questions/316781/how-to-build-query-string-with-javascript/34209399#34209399
-        if (params) {
-            let esc = encodeURIComponent;
-            query = Object.keys(params)
-                .map(k => esc(k) + '=' + esc(params[k]))
-                .join('&');
-        }
-
-        fetch(BASE_URL + api_url + `?` + query + `&apikey=${API_KEY}`)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                resolve(data);
-            })
-            .catch(function (err) {
-                console.log("dammit");
-        });
-    });
 }
